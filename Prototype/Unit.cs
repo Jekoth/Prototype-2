24d04ae01385d581978ee17a839028fdbf105d9c
Jekoth <80098768+Jekoth@users.noreply.github.com>
2022-10-31 21:34:01 +0200

    Add project files.

---
+++ Prototype/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Unit
+    {
+        public string name;
+        public int damage;
+        public int hitpoints;
+
+        public Unit(string name, int damage, int hitpoints)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.hitpoints = hitpoints;
+        }
+    }
+}
